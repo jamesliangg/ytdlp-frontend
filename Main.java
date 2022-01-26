a06465c1d1c1cd0b74fa3cf280c64c2caa08631b
James Liang <86000097+jamesliangg@users.noreply.github.com>
2022-01-25 20:03:04 -0500

    Update Main.java
    Removed original code that referenced old code

--- Main.java
+++ Main.java
@@ -1,10 +1,5 @@
 class Main {
     public static void main(String[] args) {
-      // GUI.introduction();
-      // String URL = GUI.downloadURL();
-      // System.out.println(URL);
-      // GUI.downloadOptions();
-      // GUI.resultCommand(URL);
       new UI();
     }
   }
